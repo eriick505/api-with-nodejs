354b1de096cca0fd353bcc94a992f24e1a932f41
Erick <santoserick9@gmail.com>
2021-08-29 22:06:38 -0300

    feat: added morgan instance and error treatment to routes

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const productsRouter = require("./routes/products");
 const ordersRouter = require("./routes/order");
 
+app.use(morgan("dev"));
+
 app.use("/products", productsRouter);
 app.use("/orders", ordersRouter);
 
-app.use("/test/", (req, res, next) => {
-  res.status(200).send({
-    message: "Server OK",
+// Access here when not routes are found
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    error: {
+      message: error.message,
+    },
   });
 });
 
