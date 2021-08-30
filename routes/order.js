f811bbaf50bec382cbb5d3f2d6c7da4f7db6def1
Erick <santoserick9@gmail.com>
2021-08-30 19:01:41 -0300

    feat: query two tables in only query with inner join

--- routes/order.js
+++ routes/order.js
@@ -5,7 +5,14 @@ const mysql = require("../mysql").pool;
 // GET ALL ORDERS
 router.get("/", (req, res, next) => {
   mysql.getConnection((error, conn) => {
-    const query = "SELECT * FROM orders";
+    const query = `SELECT orders.id_order,
+                          orders.quantity, 
+                          products.id_product,
+                          products.name,
+                          products.price  
+                      FROM orders
+                INNER JOIN products
+                        ON products.id_product = orders.id_product;`;
 
     if (error) return res.status(500).send({ error });
 
@@ -15,11 +22,15 @@ router.get("/", (req, res, next) => {
       if (error) return res.status(500).send({ error, response: null });
 
       const response = {
-        quantity: results.length,
+        quantityOrders: results.length,
         orders: results.map((order) => ({
           id_order: order.id_order,
-          id_product: order.id_product,
           quantity: order.quantity,
+          product: {
+            id_product: order.id_product,
+            name: order.name,
+            order: order.price,
+          },
           request: {
             type: "GET",
             description: "Get order details",
