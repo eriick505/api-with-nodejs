6491a213f6d6ca57a6044326b6648dcd49fe8848
Erick <santoserick9@gmail.com>
2021-08-29 21:48:38 -0300

    feat: added products and orders router

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
+const productsRouter = require("./routes/products");
+const ordersRouter = require("./routes/products");
+
+app.use("/products", productsRouter);
+app.use("/orders", ordersRouter);
+
+app.use("/test/", (req, res, next) => {
   res.status(200).send({
     message: "Server OK",
   });
