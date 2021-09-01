4e1722ab53f8c7446200f5c9c721b04a2f613a34
Erick <santoserick9@gmail.com>
2021-09-01 13:09:38 -0300

    feat: create router to signup users

--- routes/users.js
+++ routes/users.js
@@ -7,28 +7,39 @@ router.post("/signup", (req, res, next) => {
   mysql.getConnection((error, conn) => {
     if (error) return res.status(500).send({ error: error });
 
-    bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
-      if (errBcrypt) return res.status(500).send({ error: errBcrypt });
+    const querySelect = "SELECT * FROM users WHERE email = ?";
+    const valueSelect = [req.body.email];
 
-      const query =
-        "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
-      const values = [req.body.name, req.body.email, hash];
+    conn.query(querySelect, valueSelect, (error, results) => {
+      if (error) return res.status(500).send({ error: error });
 
-      conn.query(query, values, (error, results) => {
-        conn.release();
+      if (results.length > 0) {
+        return res.status(409).send({ message: "Already registered user" });
+      }
 
+      bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
         if (errBcrypt) return res.status(500).send({ error: errBcrypt });
 
-        const response = {
-          message: "Successfully created user",
-          userCreated: {
-            id_user: results.insertId,
-            name: req.body.name,
-            email: req.body.email,
-          },
-        };
+        const queryInsert =
+          "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+        const valuesInsert = [req.body.name, req.body.email, hash];
 
-        return res.status(201).send(response);
+        conn.query(queryInsert, valuesInsert, (error, results) => {
+          conn.release();
+
+          if (error) return res.status(500).send({ error: error });
+
+          const response = {
+            message: "Successfully created user",
+            userCreated: {
+              id_user: results.insertId,
+              name: req.body.name,
+              email: req.body.email,
+            },
+          };
+
+          return res.status(201).send(response);
+        });
       });
     });
   });
